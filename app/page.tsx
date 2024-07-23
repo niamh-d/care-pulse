3155e21b5e97f6a4b966317b260f08e1a3c770d0
Niamh <niamh.linguist@gmail.com>
2024-07-23 14:01:25 +0300

    welcome image

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import PatientForm from "@/components/forms/PatientForm";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -7,13 +9,30 @@ export default function Home() {
         <div className="sub-container max-w-[496px]">
           <Image
             src="/assets/icons/logo-full.svg"
-            alt="patient"
+            alt="CarePulse Logo"
             width={1000}
             height={1000}
             className="mb-12 h-10 w-fit"
           />
+          <PatientForm />
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              &copy; 2024 CarePulse
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
         </div>
       </section>
+
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="image of female doctor with clipboard"
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
